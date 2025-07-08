6c730847f1f819b195f221a66dc99c9b23116672
joesimpson <1324811+joesimpson@users.noreply.github.com>
2025-07-08 15:50:53 +0200

    [RULE] Remove lake cards and tokens

--- modules/php/States/PlayerTurnLakeChoiceTrait.php
+++ modules/php/States/PlayerTurnLakeChoiceTrait.php
@@ -3,9 +3,14 @@
 namespace Bga\Games\winter\States;
 
 use Bga\GameFramework\Actions\Types\IntParam;
+use Bga\Games\winter\Core\Globals;
+use Bga\Games\winter\Core\Notifications;
 use Bga\Games\winter\Exceptions\UnexpectedException;
 use Bga\Games\winter\Game;
+use Bga\Games\winter\Helpers\Utils;
+use Bga\Games\winter\Managers\Cards;
 use Bga\Games\winter\Managers\Players;
+use Bga\Games\winter\Managers\Tokens;
 
 /**
  * All about 'lakeChoice' game state 
@@ -68,7 +73,37 @@ trait PlayerTurnLakeChoiceTrait
             throw new UnexpectedException(105,"Invalid lake choice $lakeIndex");
         }
     
-        //TODO JSA ACTION EFFECT : discard cards AND tokens
+        //ACTION EFFECT : discard cards AND tokens
+        $cardsIdsToDiscard = $lakes[$lakeIndex];
+        $cards = Cards::getMany($cardsIdsToDiscard);
+        $tokens = Tokens::getBoardTokens();
+        foreach( $cards as $card){
+            //Remove TOKENS on card
+            $tokensSpotsOnCard = Utils::gridOverlappingTokensFromCard($card->getRow(), $card->getCol());
+            foreach($tokensSpotsOnCard as $coord){
+                $token = $tokens->filter(function ($token) use ($coord) {
+                    return $token->coordArray() === $coord;
+                })->first();
+                if(isset($token)){
+                    $fromLocation = $token->coordName();
+                    $token->setRow(null);
+                    $token->setCol(null);
+                    $token->setLocation(TOKEN_LOCATION_HAND);
+                    Notifications::removeToken($player,$token, $fromLocation);
+                }
+            }
+            //THEN Remove card
+            $fromLocation = $card->coordName();
+            $card->setRow(null);
+            $card->setCol(null);
+            $card->setLocation(CARD_LOCATION_DISCARD);
+            Notifications::removeCard($player,$card, $fromLocation);
+        }
+        //TODO JSA send 1 notif for all cards and tokens ?
+            
+        Globals::setLastPlayedTokens([]);
+        Globals::setLastPlayedCards([]);
+        Notifications::refreshLastPlayed(Globals::getLastPlayedDatas());
 
         // at the end of the action, move to the next state
         $this->gamestate->nextState("next");
